ae77579360fd892e6f7b5f572f54c89521bf1f27
ShiboWang912 <swang221@my.centennialcollege.ca>
2022-09-29 20:41:59 -0400

    init commit

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+//IIFE
+/* JS FILE. SHIBO WANG 301200419 SEPTEMBER 27,2022 */
+(function()
+{
+
+    function Start()
+    {
+        console.log("App Started!");
+    }
+
+    window.addEventListener('load', Start);
+})();
